d6efeb6d10faf960793fae4f79417dd88f43df51
20javierem <20javierem@gmail.com>
2022-12-13 10:50:46 -0500

    commit

--- src/main/java/com/babas/views/frames/FPrincipal.java
+++ src/main/java/com/babas/views/frames/FPrincipal.java
@@ -1109,7 +1109,7 @@ public class FPrincipal extends JFrame {
         contentPane.add(panel10, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         panel10.setBorder(BorderFactory.createTitledBorder(null, "", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
         tabbedPane = new TabbedPane();
-        tabbedPane.setOpaque(true);
+        tabbedPane.setOpaque(false);
         tabbedPane.setTabLayoutPolicy(1);
         panel10.add(tabbedPane, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         ButtonGroup buttonGroup;
